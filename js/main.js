7f163966b0a029517e162d27208eb7918ae4e658
Rimantas <belauzas@yahoo.com>
2020-09-22 15:49:00 +0300

    Kintamieji

---
+++ js/main.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// 4 + 7 = 11
+
+const pirmas = 4;
+const antras = 7;
+
+const suma = pirmas + antras;
+
+console.log(pirmas);
+console.log(antras);
+console.log(suma);
+
+let nuotaika = 'gera 👿';
+console.log(nuotaika);
+
+nuotaika = 'bloga 😇';
+console.log(nuotaika);
+console.log(nuotaika);
+console.log(nuotaika);
+console.log(nuotaika);
+console.log(nuotaika);
+console.log(nuotaika);
+console.log(nuotaika);
+
+nuotaika = '🙄';
+console.log(nuotaika);
+console.log(nuotaika);
+console.log(nuotaika);
+
+console.log(4 + 7);
+
+const rez = 2 + 2 * 2;
+console.log(rez);
+
+const hi = 'Labas';
+const mor = 'rytas';
+
+const sayHi = hi + ' ' + mor + '.';
+console.log(sayHi);
+
+const hiStrong = hi + '!';
+console.log(hiStrong);
+
+const name = 'Vardenis';
+const surname = 'Pavardenis';
+
+const fullName = name + ' ' + surname;
+console.log('Hi, I am ' + fullName);
+
+const paz1 = 1;
+const paz2 = 10;
+const paz3 = 7;
+const paz4 = 5;
+const paz5 = 8;
+
+// sarasas, array, list, masyvas
+const marks = [1, 10, 7, 8, 5, 4];
+
+const marksPaz = [paz1, paz2, paz3, paz4];
+
+console.log(marks);
+console.log(marksPaz);
+
+const fruits = ['orange', 'apple', 'banana', 'pineapple', 'kiwi'];
+
+console.log(fruits);
+
+console.log(marks);
+
+let sum = 0;
+sum = sum + marks[0];
+sum = sum + marks[1];
+sum = sum + marks[2];
+sum = sum + marks[3];
+sum = sum + marks[4];
+sum = sum + marks[5];
+const count = marks.length;
+const average = sum / count;
+
+console.log('Marks average:', sum, '/', count, '=', average);
